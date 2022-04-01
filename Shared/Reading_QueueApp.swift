ee8633d9b4cdcfacabb5850f612a8efafbc831d5
Louis <louis993546@gmail.com>
2022-04-01 23:21:27 +0200

    Initial Commit

---
+++ Shared/Reading_QueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reading_QueueApp.swift
+//  Shared
+//
+//  Created by Louis Tsai on 01.04.22.
+//
+
+import SwiftUI
+
+@main
+struct Reading_QueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
